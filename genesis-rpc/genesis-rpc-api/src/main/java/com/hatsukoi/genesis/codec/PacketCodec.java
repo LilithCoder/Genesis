5e905bf8387b154618fe29a364f53424c3d3f566
Weilin Gao <weilinga@usc.edu>
2022-06-01 05:23:02 +0800

    新建rpc模块骨架

---
+++ genesis-rpc/genesis-rpc-api/src/main/java/com/hatsukoi/genesis/codec/PacketCodec.java
@@ -0,0 +1,35 @@
+package com.hatsukoi.genesis.codec;
+
+/**
+ *
+ * @author gaoweilin
+ * @date 2022/06/01 Wed 2:14 AM
+ */
+public class PacketCodec {
+    /**
+     * magic number - used to identify whether the packet conforms to our protocol
+     * 魔数 - 用来鉴别是否接收到的数据包遵循我们的协议
+     */
+    private static final short MAGIC_NUMBER = 0x1234;
+    /**
+     * 版本号
+     */
+    private static final byte version = 1;
+    /**
+     * 数据包类型 0 - Response, 1 - Request
+     */
+    private byte packetType;
+    /**
+     * 序列化算法
+     */
+    private byte serializationAlgo;
+
+    public Object encode() {
+        return null;
+    }
+
+    public Object decode() {
+        return null;
+
+    }
+}
