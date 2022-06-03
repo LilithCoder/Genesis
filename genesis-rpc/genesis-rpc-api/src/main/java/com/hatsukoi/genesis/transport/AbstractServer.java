60dad344122f9e57b75077fc519f438942a30901
Weilin Gao <weilinga@usc.edu>
2022-06-03 17:01:42 +0800

    add registry module

--- genesis-rpc/genesis-rpc-api/src/main/java/com/hatsukoi/genesis/transport/AbstractServer.java
+++
@@ -1,8 +0,0 @@
-package com.hatsukoi.genesis.transport;
-
-/**
- * @author gaoweilin
- * @date 2022/06/01 Wed 5:20 AM
- */
-public class AbstractServer {
-}
--- genesis-rpc/genesis-rpc-netty/src/main/java/com/hatsukoi/genesis/transport/NettyClient.java
+++
@@ -1,8 +0,0 @@
-package com.hatsukoi.genesis.transport;
-
-/**
- * @author gaoweilin
- * @date 2022/06/01 Wed 5:19 AM
- */
-public class NettyClient extends AbstractClient {
-}
